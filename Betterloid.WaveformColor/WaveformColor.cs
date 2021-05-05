8b037057fa153e650edb8487a6a895690144c14d
SeleDreams <seledreams.contact@gmail.com>
2021-05-05 10:55:01 +0200

    Initial commit

---
+++ Betterloid.WaveformColor/WaveformColor.cs
@@ -0,0 +1,68 @@
+﻿using Betterloid;
+using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Windows.Media;
+#if VOCALOID6
+using Yamaha.VOCALOID;
+using Yamaha.VOCALOID.MusicalEditor;
+using Yamaha.VOCALOID.VSM;
+using VOCALOID = Yamaha.VOCALOID;
+#elif VOCALOID5
+using Yamaha.VOCALOID.VOCALOID5;
+using Yamaha.VOCALOID.VOCALOID5.MusicalEditor;
+using Yamaha.VOCALOID.VSM;
+using VOCALOID = Yamaha.VOCALOID.VOCALOID5;
+#endif
+
+namespace WaveformColor
+{
+    public class WaveformColor : IPlugin
+    {
+        public static Color DarkenColor(Color color, double factor)
+        {
+            // Ensure the factor is between 0 and 1
+            factor = Math.Max(0, Math.Min(1, factor));
+
+            // Calculate the new RGB values
+            byte r = (byte)(color.R * factor);
+            byte g = (byte)(color.G * factor);
+            byte b = (byte)(color.B * factor);
+
+            // Return the darkened color
+            return Color.FromRgb(r, g, b);
+        }
+
+        static SolidColorBrush newBrush = new SolidColorBrush(Colors.Yellow);
+        public void Startup()
+        {
+            MainWindow window = Application.Current.MainWindow as MainWindow;
+            try
+            {
+                var xMusicalEditorDiv = window.FindName("xMusicalEditorDiv") as MusicalEditorDivision;
+                var musicalEditor = xMusicalEditorDiv.DataContext as MusicalEditorViewModel;
+                var zoomScrollViewer = xMusicalEditorDiv.FindName("xPianorollViewer") as ZoomScrollViewer;
+                var view = zoomScrollViewer.Content as PianorollView;
+                var brushField = view.GetType().GetField("brushRenderedWave", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+                var penfield = view.GetType().GetField("penRenderedWave", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+
+                SolidColorBrush newBrush = new SolidColorBrush();
+                Pen newPen = new Pen(newBrush, 1.0);
+                
+                musicalEditor.UpdateViewEvent += (object sender2, VOCALOID.MusicalEditor.UpdateViewTypeFlag typeFlags, UpdateObserverNotifyEventArgs observer, object addition) =>
+                {
+                    if (musicalEditor.ActiveTrack != null )
+                    {
+                        newBrush.Color = TrackColorTable.GetTrackColor(musicalEditor.ActiveTrack.Color);
+                        newBrush.Color = DarkenColor(newBrush.Color, 0.4);
+                        penfield.SetValue(view, newPen);
+                    }
+                };
+            }
+            catch (Exception ex)
+            {
+                MessageBoxDeliverer.GeneralError("An error occured while setting the color of the waveforms ! " + ex.GetType().ToString() + " : " + ex.Message);
+            }
+        }
+    }
+}
---
+++ Setup6.bat
@@ -0,0 +1,30 @@
+@echo off
+pushd %~dp0
+goto check_Permissions
+:check_Permissions
+    net session >nul 2>&1
+    if %errorLevel% == 0 (
+        goto make_link
+    ) else (
+        echo "Please restart the setup as an administrator"
+        pause >nul
+        exit /b 1
+    )
+:make_link
+    rmdir VOCALOID6
+    set /p location="Please Specify the location of your VOCALOID6 installation folder (MAKE SURE TO USE QUOTING MARKS):"
+    if %location% == "" (
+        echo "The input is invalid !"
+        pause >nul
+        exit /b 1
+    )
+    mklink /J VOCALOID6 %location%
+    if %errorLevel% == 0 (
+        echo "Succesfully created symlink, you can now start working on your plugin"
+        pause >nul
+        exit /b 0
+    ) else (
+        echo "An error occured when making the symlink, make sure there are no errors in your path and that the symlink doesn't already exist"
+        pause >nul
+        exit /b 1
+    )
